bc9c94c196b6308e5e101b3e497eaeabda2b7689
Kunal Bhala <bhalakunal1210@gmail.com>
2024-07-07 10:46:29 +0530

    updates

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,31 +1,33 @@
 import React from 'react';
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux'
 
 function Product(props) {
     const products = useSelector(state => state.allProducts.products)
-    
-    const renderList = products.map((product)=>{
-        const { id, title, category,image,price } = product
-   
-    return (
-        <div className='four wide column'>
-            <div className='ui link cards'>
-                <div className='card'>
-                    <div className='image'>
-                        <img src={image}/>
-                    </div>
-                    <div className='content'>
-                        <div className='header'>{title}</div> 
+
+    const renderList = products.map((product) => {
+        const { id, title, category, image, price } = product
+
+        return (
+            <div className='four wide column'>
+                <div className='ui link cards'>
+                    <div className='card'>
+                        <div className='image'>
+                            <img src={image} />
+                        </div>
+                        <div className='content'>
+                            <div className='header'>{title}</div>
+                            <div className='meta price'>${price}</div>
+                            <div className='meta'>{category}</div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
 
-})
-return<>
- {renderList}
-</>
+    })
+    return <>
+        {renderList}
+    </>
 }
 
 export default Product;
